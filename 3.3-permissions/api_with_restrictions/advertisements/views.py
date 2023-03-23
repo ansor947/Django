cb7de22f5eb40ce3f8c76ba638a0588f353f9900
Andrey Sorokin <ansorpy@gmail.com>
2023-03-23 15:50:16 +0300

    API_DRF

---
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filterset_class = [AdvertisementFilter]
+ 
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated()]
+        return []
